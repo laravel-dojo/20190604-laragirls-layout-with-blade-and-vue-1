08ba46b0bfc25154ba6da4c864041a1acd2aee91
Shengyou Fan <shengyoufan@gmail.com>
2019-06-02 23:49:04 +0800

    item api for vue-cli app

--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,38 @@ Route::get('comments', function () {
         ],
     ];
 });
+
+Route::get('items', function () {
+    return [
+        [
+            'title' => 'Item 1 (from API)',
+            'price' => '$'.rand(20, 100),
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.',
+        ],
+        [
+            'title' => 'Item 2 (from API)',
+            'price' => '$'.rand(20, 100),
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.',
+        ],
+        [
+            'title' => 'Item 3 (from API)',
+            'price' => '$'.rand(20, 100),
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.',
+        ],
+        [
+            'title' => 'Item 4 (from API)',
+            'price' => '$'.rand(20, 100),
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.',
+        ],
+        [
+            'title' => 'Item 5 (from API)',
+            'price' => '$'.rand(20, 100),
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.',
+        ],
+        [
+            'title' => 'Item 6 (from API)',
+            'price' => '$'.rand(20, 100),
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.',
+        ],
+    ];
+});
