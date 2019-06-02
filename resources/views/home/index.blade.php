ccb0cdeb7b872935fb7406391ca6455e8341ecd4
Shengyou Fan <shengyoufan@gmail.com>
2019-06-02 21:44:12 +0800

    build home.index

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" value="{{ csrf_token() }}" />
+
+    <title>Hello, world!</title>
+
+    <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+<!-- Start Here -->
+<h1>首頁</h1>
+
+<script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
